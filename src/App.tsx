0a5a1e7bb15ace6cd6dca89c33d3e9619c1c5bac
xianengqi <xianengqi@gmail.com>
2020-05-25 19:21:16 +0800

    Add custom style

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+
   return (
     <div className="App">
       <header className="App-header">
