8820ee5c65c47e6b2cab1dce765b477382e18cb0
xianengqi <xianengqi@gmail.com>
2020-05-28 03:51:09 +0800

    Add subMenu test case

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import {render, RenderResult, fireEvent, cleanup} from '@testing-library/react'
+import {render, RenderResult, fireEvent, cleanup, wait} from '@testing-library/react'
 
 import Menu, {MenuProps} from './menu'
 import MenuItem from './menuItem'
+import SubMenu from './subMenu'
 
 const testProps: MenuProps = {
   defaultIndex: '0',
@@ -19,14 +20,32 @@ const generateMenu = (props: MenuProps) => {
       <MenuItem>active</MenuItem>
       <MenuItem disabled>disabled</MenuItem>
       <MenuItem>xyz</MenuItem>
+      <SubMenu title="dropdown">
+      <MenuItem>drop1</MenuItem>
+      </SubMenu>
     </Menu>
   )
+}
+const createStyleFile = () => {
+  const cssFile: string = `
+    .turing-submenu {
+      display: none;
+    }
+    .turing-submenu.menu-opened {
+      display: block;
+    }
+  `
+  const style = document.createElement('style')
+  style.type = 'text/css'
+  style.innerHTML = cssFile
+  return style
 }
 let wrapper: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement
 describe('测试 Menu 和 MenuItem 组件', () => {
   // 通用的元素可以放在`beforEach`这个钩子函数里面
   beforeEach(() => {
     wrapper = render(generateMenu(testProps))
+    wrapper.container.append(createStyleFile())
     menuElement = wrapper.getByTestId('test-menu')
     activeElement = wrapper.getByText('active')
     disabledElement = wrapper.getByText('disabled')
@@ -34,7 +53,8 @@ describe('测试 Menu 和 MenuItem 组件', () => {
   it('测试Menu和MenuItem默认props', () => {
     expect(menuElement).toBeInTheDocument()
     expect(menuElement).toHaveClass('turing-menu test')
-    expect(menuElement.getElementsByTagName('li').length).toEqual(3)
+    // expect(menuElement.getElementsByTagName('li').length).toEqual(3)
+    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4)
     expect(activeElement).toHaveClass('menu-item is-active')
     expect(disabledElement).toHaveClass('menu-item is-disabled')
   })
@@ -46,12 +66,26 @@ describe('测试 Menu 和 MenuItem 组件', () => {
     expect(testProps.onSelect).toHaveBeenCalledWith('2')
     fireEvent.click(disabledElement)
     expect(disabledElement).not.toHaveClass('is-active')
-    expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
+    expect(testProps.onSelect).not.toHaveBeenCalledWith('1')
   })
   it('测试是否切换对应的menu横向或者是纵向菜单', () => {
     cleanup()
     const wrapper = render(generateMenu(testVerProps))
     const menuElement = wrapper.getByTestId('test-menu')
     expect(menuElement).toHaveClass('menu-vertical')
+  })
+  it('测试默认的横向dorpdown模式', async() => {
+    expect(wrapper.queryByText('drop1')).not.toBeVisible()
+    const dropdownElement = wrapper.getByText('dropdown')
+    fireEvent.mouseEnter(dropdownElement)
+    await wait(() => {
+      expect(wrapper.queryByText('drop1')).toBeVisible()
+    })
+    fireEvent.click(wrapper.getByText('drop1'))
+    expect(testProps.onSelect).toHaveBeenCalledWith('3-0')
+    fireEvent.mouseLeave(dropdownElement)
+    await wait(() => {
+      expect(wrapper.queryByText('drop1')).not.toBeVisible()
+    })
   })
 })
