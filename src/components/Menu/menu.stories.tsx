2ff80bd71bc342631b49439801d42f209a0a5928
xianengqi <xianengqi@gmail.com>
2020-06-16 23:19:22 +0800

    test travis

--- src/components/Menu/menu.stories.tsx
+++ src/components/Menu/menu.stories.tsx
@@ -1,45 +1,67 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-
-import Menu from './menu'
-import MenuItem from './menuItem'
-import SubMenu from './subMenu'
+import Menu from './index'
 
 export const defaultMenu = () => (
-  <Menu defaultIndex='0' onSelect={action('selected!')}>
-    <MenuItem>test1</MenuItem>
-    <MenuItem>test2</MenuItem>
-    <MenuItem>test3</MenuItem>
-    <SubMenu title="下拉选项">
-      <MenuItem>下拉选项1</MenuItem>
-    </SubMenu>
+  <Menu defaultIndex='0' onSelect={action('selected!')} >
+    <Menu.Item>
+      cool link
+    </Menu.Item>
+    <Menu.Item>
+      cool link 2
+    </Menu.Item>
+    <Menu.Item disabled>
+      disabled
+    </Menu.Item> 
+    <Menu.SubMenu title="下拉选项">
+      <Menu.Item>
+        下拉选项一
+      </Menu.Item>
+      <Menu.Item>
+        下拉选项二
+      </Menu.Item>    
+    </Menu.SubMenu>
   </Menu>
 )
-
 export const clickMenu = () => (
   <Menu defaultIndex='0' onSelect={action('selected!')} mode="vertical">
-    <MenuItem>test1</MenuItem>
-    <MenuItem>test2</MenuItem>
-    <MenuItem>test3</MenuItem>
-    <SubMenu title="下拉选项">
-    <MenuItem>demo1</MenuItem>
-    </SubMenu>
+    <Menu.Item>
+      cool link
+    </Menu.Item>
+    <Menu.Item>
+      cool link 2
+    </Menu.Item>
+    <Menu.SubMenu title="点击下拉选项">
+      <Menu.Item>
+        下拉选项一
+      </Menu.Item>
+      <Menu.Item>
+        下拉选项二
+      </Menu.Item>    
+    </Menu.SubMenu>
   </Menu>
 )
-
-export const openMenu = () => (
-  <Menu defaultIndex='0' onSelect={action('selected!')} mode="vertical" defaultOpenSubMenus={['3']}>
-    <MenuItem>test1</MenuItem>
-    <MenuItem>test2</MenuItem>
-    <MenuItem>test3</MenuItem>
-    <SubMenu title="下拉选项">
-    <MenuItem>demo1</MenuItem>
-    </SubMenu>
+export const openedMenu = () => (
+  <Menu defaultIndex='0' onSelect={action('selected!')} mode="vertical" defaultOpenSubMenus={['2']}>
+    <Menu.Item>
+      cool link
+    </Menu.Item>
+    <Menu.Item>
+      cool link 2
+    </Menu.Item>
+    <Menu.SubMenu title="默认展开下拉选项">
+      <Menu.Item>
+        下拉选项一
+      </Menu.Item>
+      <Menu.Item>
+        下拉选项二
+      </Menu.Item>    
+    </Menu.SubMenu>
   </Menu>
 )
 
 storiesOf('Menu', module)
-  .add('Menu', defaultMenu)
-  .add('纵向的Menu', clickMenu)
-  .add('默认展开的纵向Menu', openMenu)
+.add('Menu', defaultMenu )
+.add('纵向的 Menu', clickMenu)
+.add('默认展开的纵向 Menu', openedMenu)
